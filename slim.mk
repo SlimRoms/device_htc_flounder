f1d17b5275801b79501632f4cf9a3f9695c65f13
Nicholas Flintham <nick@flinny.org>
2015-01-26 13:00:46 +0000

    Slim : add some APN's for LTE variant
    Change-Id: I7ab93feec3a785d9971c9e6224b6a1e9e63fa9ab

--- slim.mk
+++ slim.mk
@@ -4,6 +4,6 @@ $(call inherit-product, device/htc/flounder/full_flounder.mk)
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit some common slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/slim/config/common_full_tablet_lte.mk)
 
 PRODUCT_NAME := slim_flounder
