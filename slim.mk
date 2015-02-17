fbe11a640d047ffffff3797b0f690a3e3fd3f6b8
Simon Sickle <simon@simonsickle.com>
2015-02-16 19:20:39 -0600

    flounder: Add call for vendor, if it exists
    Change-Id: I1cc6bf719d410ef83e130911b39edc97dc9b641a
    Signed-off-by: Simon Sickle <simon@simonsickle.com>

--- slim.mk
+++ slim.mk
@@ -11,6 +11,8 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 # Inherit device configuration
 $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
 
+$(call inherit-product-if-exists, vendor/htc/flounder/flounder-vendor-blobs.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := slim_flounder
 PRODUCT_BRAND := google
