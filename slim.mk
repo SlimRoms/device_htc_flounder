1e96e59085c8dd9b83897c50c9abe914f21174d1
Nicholas Flintham <nick@flinny.org>
2015-10-16 17:27:45 +0200

    Fix build: add slim.mk
    Change-Id: I919e095d4725d95ee26063bcee471f9eedc90f22

---
+++ slim.mk
@@ -0,0 +1,46 @@
+#  Custom Changes
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2048
+TARGET_SCREEN_WIDTH := 1536
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/flounder/aosp_flounder.mk)
+
+$(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
+
+# Inline kernel building
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/htc/flounder
+TARGET_KERNEL_CONFIG := flounder_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
+TARGET_PREBUILT_KERNEL := false
+
+# Extra Packages
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras
+
+# Slim Overlays
+DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-slim
+
+# Enable USB OTG (CAF commit to Settings)
+ADDITIONAL_BUILD_PROPERTIES += \
+    persist.sys.isUsbOtgEnabled=true
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=flounder \
+    BUILD_FINGERPRINT=google/volantis/flounder:5.1.1/LMY48I/2074855:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 5.1.1 LMY48I 2074855 release-keys"
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := slim_flounder
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 9
