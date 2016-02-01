04cd85efe1b33dbccfa6590f859ded74e1783acb
Simon Sickle <simon@simonsickle.com>
2016-02-01 22:04:38 +0100

    flounder: Add call for vendor, if it exists
    Change-Id: I6e479583e8c319d1d5b3362fa0efd28c97c75565

--- aosp_flounder.mk
+++ aosp_flounder.mk
@@ -34,6 +34,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/htc/flounder/product.mk)
 $(call inherit-product, device/htc/flounder/device-lte.mk)
+$(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
 $(call inherit-product-if-exists, vendor/htc/flounder_lte/device-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
