1089313d942d8ea849ab4f2b1fc8eba7ea9d2bc8
Joshndroid <fmxjoshiewah@gmail.com>
2016-02-11 18:59:01 +1000

    Snap : Build and Inport Snap Camera
    Change-Id: Ie97a7904355a4ad67005be8bb33a6906658cd7eb
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- device.mk
+++ device.mk
@@ -66,6 +66,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# Snap
+PRODUCT_PACKAGES += \
+    Snap
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
