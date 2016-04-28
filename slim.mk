be04d083b9ef480c793968eeeeee6a64e4e8e7eb
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-04-28 20:31:04 +0200

    Slimify / Start with MOB30D fingerprint
    Change-Id: I3e5f1f185e82f6145f882013388ab77c3d545753
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

---
+++ slim.mk
@@ -0,0 +1,27 @@
+# Release name
+PRODUCT_RELEASE_NAME := flounder
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1536
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/flounder/aosp_flounder.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-slim
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := slim_flounder
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 9
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=flounder \
+    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MOB30D/2704746:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MOB30D 2704746 release-keys"
