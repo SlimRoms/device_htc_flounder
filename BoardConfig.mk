8692981ac8c831d122adb7af18881f51ccd16e76
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-04-28 20:33:08 +0200

    Build kernel in-line
    Change-Id: I9b3994fef14357a7f61a1b8399df5ec487618aa3
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,13 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := denver
 
+# Inline kernel building
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/htc/flounder
+TARGET_KERNEL_CONFIG := flounder_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
 # Disable emulator for "make dist" until there is a 64-bit qemu kernel
 BUILD_EMULATOR := false
 
@@ -123,3 +130,4 @@ USE_CLANG_PLATFORM_BUILD := true
 
 # Use the non-open-source parts, if they're present
 -include vendor/htc/flounder/BoardConfigVendor.mk
+
