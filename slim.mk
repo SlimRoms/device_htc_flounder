492eb0a15721e0ea0768df3f872f81e63f53eacc
Josue Rivera <prbassplayer@gmail.com>
2017-03-07 17:07:01 +0100

    Flounder: N4F26T
    Change-Id: I219bf13f18b79edd67a0524c9452d90e40ed50a5
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -20,5 +20,5 @@ PRODUCT_MODEL := Nexus 9
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F26Q/3623443:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F26Q 3623443 release-keys"
+    BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N4F26T/3687331:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 7.1.1 N4F26T 3687331 release-keys"
