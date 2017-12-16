LineageOS/android_device_htc_flounder

flounder: Redo the inclusion of device-specific overlays
 * Apparently the inclusion of the overlays targeted for the WiFi-only variant
   has been broken in our builds since... ever.

 * Gather overlay inclusion all in one place since we already have
   the TARGET_PRODUCT checks there.

Change-Id: I2400dd0956727b1d0a3ee09c7d76e9ddcae0d0a1

device.mk
@@ -156,13 +156,14 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 PRODUCT_CHARACTERISTICS := tablet,nosdcard
 
-ifneq ($(filter volantis volantisf, $(TARGET_PRODUCT)),)
+ifneq ($(filter lineage_flounder volantis volantisf, $(TARGET_PRODUCT)),)
 # Wifi-Only overlays.
 DEVICE_PACKAGE_OVERLAYS := \
     $(LOCAL_PATH)/wifi_only_overlay \
     $(LOCAL_PATH)/overlay
 else
 DEVICE_PACKAGE_OVERLAYS := \
+    $(LOCAL_PATH)/lte_only_overlay \
     $(LOCAL_PATH)/overlay
 endif
 

SlimRoms/device_htc_flounder

device.mk
@@ -163,13 +163,14 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 PRODUCT_CHARACTERISTICS := tablet,nosdcard
 
-ifneq ($(filter volantis volantisf, $(TARGET_PRODUCT)),)
+ifneq ($(filter slim_flounder volantis volantisf, $(TARGET_PRODUCT)),)
 # Wifi-Only overlays.
 DEVICE_PACKAGE_OVERLAYS := \
     $(LOCAL_PATH)/wifi_only_overlay \
     $(LOCAL_PATH)/overlay
 else
 DEVICE_PACKAGE_OVERLAYS := \
+    $(LOCAL_PATH)/lte_only_overlay \
     $(LOCAL_PATH)/overlay
 endif
 