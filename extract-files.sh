LineageOS/android_device_htc_flounder

flounder: Add support for common and device-specific blobs
 * Up until now the vendor blobs were shipped in its dedicated partition.
   Since some modifications to vendor are now required in Oreo,
   we will now need to differentiate between Wi-Fi only and LTE variants.

Change-Id: I1895f7fe03786bb95cb350fdb65d38952eeaafbd

extract-files.sh
@@ -18,7 +18,10 @@
 
 set -e
 
-DEVICE=flounder
+if [ -z "$DEVICE" ]; then
+    DEVICE=flounder
+fi
+DEVICE_COMMON=flounder-common
 VENDOR=htc
 
 # Load extract_utils and do some sanity checks
@@ -56,9 +59,16 @@ if [ -z "$SRC" ]; then
     SRC=adb
 fi
 
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+# Initialize the helper for common device
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
 
-extract "$MY_DIR"/device-proprietary-files.txt "$SRC" "$SECTION"
+extract "$MY_DIR"/common-proprietary-files.txt "$SRC" "$SECTION"
+
+if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
+    # Reinitialize the helper for device
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+
+    extract "$MY_DIR"/../$DEVICE/device-proprietary-files.txt "$SRC" "$SECTION"
+fi
 
 "$MY_DIR"/setup-makefiles.sh

setup-makefiles.sh
@@ -18,7 +18,10 @@
 
 set -e
 
-DEVICE=flounder
+if [ -z "$DEVICE" ]; then
+    DEVICE=flounder
+fi
+DEVICE_COMMON=flounder-common
 VENDOR=htc
 
 INITIAL_COPYRIGHT_YEAR=2016
@@ -36,13 +39,24 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+# Initialize the helper for common device
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
-# Copyright headers and guards
-write_headers
+# Copyright headers and common guards
+write_headers "flounder flounder_lte"
 
-write_makefiles "$MY_DIR"/device-proprietary-files.txt
+write_makefiles "$MY_DIR"/common-proprietary-files.txt
 
-# Finish
 write_footers
+
+if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
+    # Reinitialize the helper for device
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+    # Copyright headers and guards
+    write_headers
+
+    write_makefiles "$MY_DIR"/../$DEVICE/device-proprietary-files.txt
+
+    write_footers
+fi

SlimRoms/device_htc_flounder

extract-files.sh
@@ -18,7 +18,10 @@
 
 set -e
 
-DEVICE=flounder
+if [ -z "$DEVICE" ]; then
+    DEVICE=flounder
+fi
+DEVICE_COMMON=flounder-common
 VENDOR=htc
 
 # Load extract_utils and do some sanity checks
@@ -55,9 +58,17 @@ if [ -z "$SRC" ]; then
     SRC=adb
 fi
 
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$SLIM_ROOT" false "$CLEAN_VENDOR"
+# Initialize the helper for common device
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$SLIM_ROOT" true "$CLEAN_VENDOR"
 
-extract "$MY_DIR"/device-proprietary-files.txt "$SRC" "$SECTION"
+extract "$MY_DIR"/common-proprietary-files.txt "$SRC" "$SECTION"
+
+if [ -s "$MY_DIR"/proprietary-files.txt ] || [ -s "$MY_DIR"/../$DEVICE/device-proprietary-files.txt ]; then
+    # Reinitialize the helper for device
+    setup_vendor "$DEVICE" "$VENDOR" "$SLIM_ROOT" false "$CLEAN_VENDOR"
+
+    extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+    extract "$MY_DIR"/../$DEVICE/device-proprietary-files.txt "$SRC" "$SECTION"
+fi
 
 "$MY_DIR"/setup-makefiles.sh

setup-makefiles.sh
@@ -18,7 +18,10 @@
 
 set -e
 
-DEVICE=flounder
+if [ -z "$DEVICE" ]; then
+    DEVICE=flounder
+fi
+DEVICE_COMMON=flounder-common
 VENDOR=htc
 
 INITIAL_COPYRIGHT_YEAR=2016
@@ -36,13 +39,25 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$SLIM_ROOT"
+# Initialize the helper for common device
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$SLIM_ROOT" true
 
-# Copyright headers and guards
-write_headers
+# Copyright headers and common guards
+write_headers "flounder flounder_lte"
 
-write_makefiles "$MY_DIR"/device-proprietary-files.txt
+write_makefiles "$MY_DIR"/common-proprietary-files.txt
 
-# Finish
 write_footers
+
+if [ -s "$MY_DIR"/proprietary-files.txt ] || [ -s "$MY_DIR"/../$DEVICE/device-proprietary-files.txt ]; then
+    # Reinitialize the helper for device
+    setup_vendor "$DEVICE" "$VENDOR" "$SLIM_ROOT"
+
+    # Copyright headers and guards
+    write_headers
+
+    write_makefiles "$MY_DIR"/proprietary-files.txt
+    write_makefiles "$MY_DIR"/../$DEVICE/device-proprietary-files.txt
+
+    write_footers
+fi